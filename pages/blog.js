8dab46e996d604f85b783c9aaedcf95164fbe2e5
murat-so <81262445+murat-so@users.noreply.github.com>
2021-03-29 16:58:35 +0300

    Update Index

--- pages/blog.js
+++ pages/blog.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import Link from 'next/link'
 
-import { Box,Badge,Heading,Text } from "@chakra-ui/react"
+import { Box,Badge,Heading,Text, Container, Image} from "@chakra-ui/react"
 
 export default function Home() {
   return (
@@ -11,23 +11,85 @@ export default function Home() {
         <title>Blog | {siteTitle}</title>
       </Head>
 
-      <section>
-        <Link href="blog/first-post">
-          <Box cursor="pointer" mb="3" p={3} shadow="md" borderWidth="1px" flex="1" borderRadius="md">
-              <Heading fontSize="md">First Post <Badge borderRadius="full" ml="1" px="2" colorScheme="red">New</Badge></Heading>
-          </Box>
-        </Link>{/* #post */}
-        <Link href="blog/first-post">
-          <Box cursor="pointer" mb="3" p={3} shadow="md" borderWidth="1px" flex="1" borderRadius="md">
-              <Heading fontSize="md">First Post <Badge borderRadius="full" ml="1" px="2">2 days ago</Badge></Heading>
-          </Box>
-        </Link>{/* #post */}
-        <Link href="blog/first-post">
-          <Box cursor="pointer" mb="3" p={3} shadow="md" borderWidth="1px" flex="1" borderRadius="md">
-              <Heading fontSize="md">First Post <Badge borderRadius="full" ml="1" px="2">2 months ago</Badge></Heading>
-          </Box>
-        </Link>{/* #post */}
-      </section>
+      <Container mt="3">{/* POSTS: */}
+
+          <Box p={0} mb={3} display={{ md: "flex" }} p={2} shadow="md" borderWidth="1px" flex="1" borderRadius="md">
+            <Box flexShrink={0}>
+              <Image
+                borderRadius="md"
+                width={{ md: 110 }}
+                src="https://avatars.githubusercontent.com/u/81262445?v=4"
+                alt="Woman paying for a purchase"
+              />
+            </Box>
+            <Box mt={{ base: 4, md: 0 }} ml={{ md: 6 }}>
+              <Text
+                fontWeight="bold"
+                textTransform="uppercase"
+                fontSize="xs"
+                letterSpacing="wide"
+                color="teal.600"
+                mt="2">
+                Software Development 
+              </Text>
+              <Link href="blog/first-post">
+                <Text cursor="pointer" fontSize="xm">Legendary GT Computing Instructor Bill Leahy Retires</Text>
+              </Link>
+            </Box>
+          </Box> {/* post */}
+
+          <Box p={0} mb={3} display={{ md: "flex" }} p={2} shadow="md" borderWidth="1px" flex="1" borderRadius="md">
+            <Box flexShrink={0}>
+              <Image
+                borderRadius="md"
+                width={{ md: 110 }}
+                src="https://avatars.githubusercontent.com/u/81262445?v=4"
+                alt="Woman paying for a purchase"
+              />
+            </Box>
+            <Box mt={{ base: 4, md: 0 }} ml={{ md: 6 }}>
+              <Text
+                fontWeight="bold"
+                textTransform="uppercase"
+                fontSize="xs"
+                letterSpacing="wide"
+                color="teal.600"
+                mt="2" mb="1">
+                Software Development
+              </Text>
+              <Link href="blog/first-post">
+                <Text cursor="pointer" fontSize="xm">Legendary GT Computing Instructor Bill Leahy Retires</Text>
+              </Link>
+            </Box>
+          </Box> {/* post */}
+
+          <Box p={0} mb={3} display={{ md: "flex" }} p={2} shadow="md" borderWidth="1px" flex="1" borderRadius="md">
+            <Box flexShrink={0}>
+              <Image
+                borderRadius="md"
+                width={{ md: 110 }}
+                src="https://avatars.githubusercontent.com/u/81262445?v=4"
+                alt="Woman paying for a purchase"
+              />
+            </Box>
+            <Box mt={{ base: 4, md: 0 }} ml={{ md: 6 }}>
+              <Text
+                fontWeight="bold"
+                textTransform="uppercase"
+                fontSize="xs"
+                letterSpacing="wide"
+                color="teal.600"
+                mt="2" mb="1">
+                Software Development
+              </Text>
+              <Link href="blog/first-post">
+                <Text cursor="pointer" fontSize="xm">Legendary GT Computing Instructor Bill Leahy Retires</Text>
+              </Link>
+            </Box>
+          </Box> {/* post */}
+         
+          
+      </Container> 
     </Layout>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,10 @@ import { Container } from "@chakra-ui/react"
 import { Text,IconButton, Button, ButtonGroup, Divider } from "@chakra-ui/react"
 
 import { FaTwitter, FaGithub, FaTelegram, FaLinkedin, FaLink } from "react-icons/fa"
-import { AiFillMail } from "react-icons/ai"
-import { FaMugHot } from "react-icons/fa"
+import { AiFillMail,AiFillPushpin } from "react-icons/ai"
+import { FaMugHot,FaBlogger } from "react-icons/fa"
 
-import { Box,Badge,Heading } from "@chakra-ui/react"
+import { Box,Badge,Heading,Image } from "@chakra-ui/react"
 
 
 export default function Home() {
@@ -22,32 +22,35 @@ export default function Home() {
       
       <Container>{/* BIO: */}
         <Text bgGradient="linear(to-l, #7928CA,#FF0080)" bgClip="text" fontSize="6xl" fontWeight="extrabold">
-        Hello, I'm Murat.
+            Hello, I'm Murat.
         </Text>
         <Text fontSize="md">
             I living in San Francisco. I interested SwiftUI, NextJS and PHP. And I'm writing something.
         </Text>
       </Container>
       
-      <Container mt="5">{/* SOCIAL BUTTONS: */}
-        <IconButton aria-label="Twitter" icon={<FaTwitter />}/>{' '}
-        <Button leftIcon={<FaGithub />}>Github</Button>{' '}
-        <Button leftIcon={<AiFillMail />}>Send Me Mail </Button>{' '}
+      <Container mt="10">{/* SOCIAL BUTTONS: */}
+          <Button>Photos</Button>{' '}
+          <Button>Books</Button>{' '}
+          <Button>Projects</Button>{' '}
+          <Link href={'blog'}><Button bgGradient="linear(to-l, #7928CA,#FF0080)" _hover={{ opacity:.9, bg: "linear(to-l, #7928CA,#FF0080)" }} _active={{bg: "linear(to-l, #7928CA,#FF0080)"}} color="white">SEE MY BLOG</Button></Link>
       </Container>
 
-      <Container mt="10">{/* POSTS: */}
-          <Text fontSize="md" mb="3" bgGradient="linear(to-l, #7928CA,#FF0080)" bgClip="text">My posts </Text> {/* #TITLE */}
-            <Link href="blog/first-post">
-              <Box cursor="pointer" mb="3" p={3} shadow="md" borderWidth="1px" flex="1" borderRadius="md">
-                <Heading fontSize="md">Legendary GT Computing Instructor Bill Leahy Retires <Badge borderRadius="full" ml="1" px="2" py="1" colorScheme="red">New</Badge></Heading>
-              </Box>
-            </Link>{/* #post */}
-            <Link href="blog/first-post">
-              <Box cursor="pointer" mb="3" p={3} shadow="md" borderWidth="1px" flex="1" borderRadius="md">
-                <Heading fontSize="md">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum aperiam, quos provident.<Badge borderRadius="full" ml="1" px="2" py="1">29.03.2021</Badge></Heading>
-              </Box>
-            </Link>{/* #post */}
+
+      <Container mt="10">{/* REVİEWS: */}
+            <Link href="reviews">
+                <Button mb="3" p={3} shadow="md" fontSize="sm" w="100%" variant="outline" leftIcon={<FaTwitter />}>Twitter</Button>
+            </Link>{/* #menu i*/}
+            <Link href="photos">
+                <Button mb="3" p={3} shadow="md" fontSize="sm" w="100%" variant="outline" leftIcon={<FaGithub />}>Github</Button>
+            </Link>{/* #menu i*/}
+            <Link href="photos">
+                <Button mb="3" p={3} shadow="md" fontSize="sm" w="100%" variant="outline" leftIcon={<AiFillMail />}>Mail</Button>
+            </Link>{/* #menu i*/}
       </Container>
+
+
+
       
       
     </Layout>
